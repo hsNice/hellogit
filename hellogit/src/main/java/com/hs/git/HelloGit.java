9c0276f37b1df12d22c53b7f05560e524994a512
hsNice <14736655032@163.com>
2020-10-10 21:35:08 +0800

    添加文件

---
+++ hellogit/src/main/java/com/hs/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.hs.git;
+
+/**
+ * @author HS
+ * @Description
+ * @create 2020-10-10 21:28
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
