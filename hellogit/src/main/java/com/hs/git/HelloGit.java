ca8a771dda305089c4122dd665bfe1598937bfbf
hsNice <14736655032@163.com>
2020-10-10 21:48:40 +0800

    主干添加

--- hellogit/src/main/java/com/hs/git/HelloGit.java
+++ hellogit/src/main/java/com/hs/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("创建分支！");
+        System.out.println("主干添加@！");
     }
 }
